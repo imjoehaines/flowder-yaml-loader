a3966d13fc4f352d70b813e4736cdca6cdcaaff6
Joe Haines <hello@joehaines.co.uk>
2017-04-07 21:09:39 +0100

    Add YAML loader

---
+++ src/YamlLoader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Imjoehaines\FlowderYaml;
+
+use Symfony\Component\Yaml\Yaml;
+use Imjoehaines\Flowder\Loader\LoaderInterface;
+
+final class YamlLoader implements LoaderInterface
+{
+    /**
+     * Loads the given Yaml file
+     *
+     * @param string $file
+     * @return iterable
+     */
+    public function load($file)
+    {
+        $table = pathinfo($file, PATHINFO_FILENAME);
+
+        $yaml = file_get_contents($file);
+
+        $data = Yaml::parse($yaml);
+
+        yield $table => $data;
+    }
+}
---
+++ tests/Integration/YamlLoaderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Imjoehaines\FlowderYaml\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+use Imjoehaines\FlowderYaml\YamlLoader;
+
+final class YamlLoaderTest extends TestCase
+{
+    public function testItReturnsParsedYamlFromTheGivenFilePath()
+    {
+        $loader = new YamlLoader();
+        $data = $loader->load(__DIR__ . '/../data/one.yml');
+
+        $actual = iterator_to_array($data);
+        $expected = [
+            'one' => [
+                'column_1' => 'value 1',
+                'column_2' => 2,
+                'column_3' => 'three',
+            ],
+        ];
+
+        $this->assertSame($expected, $actual);
+    }
+}
